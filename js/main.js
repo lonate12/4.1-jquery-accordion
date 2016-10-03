2231e489467492e99579127415397882a0b6f3c5
Luis Rene Onate <lonate12@gmail.com>
2016-10-03 18:39:10 -0400

    Completed 1st jQuery assignment.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 (function($){
   $(function(){
-    console.log$(('.active'));
 
+    toggleSlide();
+
+    $('.header').on('click', checkClass);
+
+    function toggleSlide(){
+      $('.active').next().slideDown();
+      $('.not-active').next().slideUp();
+    }
+
+    function checkClass(){
+      $('.active').removeClass('active').addClass('not-active');
+
+      if($(this).hasClass('not-active')){
+        $(this).removeClass('not-active');
+        $(this).addClass('active');
+      }
+
+      toggleSlide();
+    }
 
 
 
